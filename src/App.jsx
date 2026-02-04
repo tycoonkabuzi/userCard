d289851c2a4640002177c9929a7e49ae828ffa75
Tycoon <kabuzitycoon@gmail.com>
2026-02-04 13:10:08 +0100

    the visual part of the card is done

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import "./App.css";
 import UserCard from "./components/UserCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <UserCard name="Tycoon" email="kabuzitycoon@gmail.com" role="user" />
+      <UserCard name="Tycoon" email="kabuzitycoon@gmail.com" role="standard" />
     </>
   );
 }
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,10 +1,17 @@
+import styles from "./style.module.css";
+
 const UserCard = ({ name, email, role }) => {
   return (
-    <>
-      <p>
-        My name is :{name} and my email is {email}
-      </p>
-    </>
+    <div className={styles.background}>
+      <h1>User Data</h1>
+      <ul>
+        <li> Name: {name}</li>
+        <li> email: {email}</li>
+        <li>role :{role}</li>
+      </ul>
+
+      <button className={styles.button}>Promote</button>
+    </div>
   );
 };
 export default UserCard;
