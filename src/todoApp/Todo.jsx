2f06acfe08314467c847f2856224e96e80e83ddb
Tycoon <kabuzitycoon@gmail.com>
2026-02-10 14:10:28 +0100

    able to add the todo in a list below

--- src/todoApp/Todo.jsx
+++ src/todoApp/Todo.jsx
@@ -1,4 +1,48 @@
+import { useReducer, useState } from "react";
+import style from "./todo.module.css";
 const Todo = () => {
-  return <div> Todo App</div>;
+  const [text, setText] = useState("");
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "add":
+        return [
+          ...state,
+          { id: Date.now, text: action.payload, completed: false },
+        ];
+
+      default:
+        return state;
+    }
+  };
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    return dispatch({ type: "add", payload: text });
+  };
+
+  const initialState = [];
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <label htmlFor="">Todo: </label>
+      <input
+        placeholder="What is on your mind today?"
+        onChange={handleChange}
+      />{" "}
+      <button className={style.button} onClick={handleSubmit}>
+        {" "}
+        validate{" "}
+      </button>
+      {state.map((todo) => (
+        <p key={todo.id}>
+          {todo.text} <button className={style.button}>Erase</button>{" "}
+          <button className={style.button}>Delete</button>
+        </p>
+      ))}
+    </div>
+  );
 };
 export default Todo;
